543b26f31f91c3b844be67ffa7f9f46e325d4436
Jesus Palacio <palacioj104@gmail.com>
2024-10-20 14:48:36 -0500

    Fin seccion 13

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-pages/layout-pages.component';
+import { LoguinPageComponent } from './pages/loguin-pages/loguin-pages.component';
+import { ReguisterPageComponent } from './pages/reguister-page/reguister-page.component';
 
-const routes: Routes = [];
+// localhost:4200/auth/
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {path: 'loguin', component: LoguinPageComponent},
+      {path: 'new-account', component: ReguisterPageComponent},
+      {path: '**', redirectTo: 'loguin'},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
