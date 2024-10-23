272c89a07df4a70327eda55fe8b8803428372ca6
Jesus Palacio Yancy <jpalacio@transportationamerica.com>
2024-10-23 17:03:06 -0500

    Inicio seccion 15

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interfaces';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -7,9 +10,44 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public heroForm = new FormGroup({
+    id:              new FormControl<string>(''),
+    superhero:       new FormControl<string>('', {nonNullable: true}),
+    publisher:       new FormControl<Publisher>( Publisher.DCComics),
+    alter_ego:       new FormControl<string>(''),
+    first_appearance:new FormControl<string>(''),
+    characters:      new FormControl<string>(''),
+    alt_img:         new FormControl<string>(''),
+  });
+
+  constructor(private heroesService: HeroesService){}
+
+  get currenHero(): Hero {
+    const hero = this.heroForm.value;
+    return hero as Hero;
+  }
+
   public publishers = [
     { id: 'dc_comics', desc: 'DC - Comics' },
     { id: 'marvel-comics', desc: 'Marvel - Comics' }
   ];
+
+  onSubmint():void {
+    if(this.heroForm.invalid) return;
+
+    if(this.currenHero.id) {
+      this.heroesService.updateHero(this.currenHero)
+        .subscribe( hero => {
+
+        });
+      return;
+    }
+
+    this.heroesService.addHero(this.currenHero)
+      .subscribe( hero => {
+
+      });
+
+  }
 
 }
